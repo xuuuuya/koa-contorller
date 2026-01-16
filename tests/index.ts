637d021e4eb474bff0d7f118dd2562a40bb2e8bf
xuuuuya <xuuuuya@163.com>
2026-01-16 17:38:13 +0800

    添加配置项

--- tests/index.ts
+++ tests/index.ts
@@ -1,6 +1,6 @@
 import { Context } from 'koa';
-import { KoaContainerApp, Controller, Get, Post, Query, Body, Param, Ctx, Service } from '../src'; // 引入 Service 装饰器
-import bodyParser from "koa-bodyparser";
+import { KoaContainerApp, Controller, Get, Post, Query, Body, Param, Ctx, Service } from '../dist/index'; // 引入 Service 装饰器
+import bodyParser from 'koa-bodyparser';
 @Service() // 默认单例，等价于 @Service('singleton')
 class UserService {
     private users = [
@@ -9,43 +9,42 @@ class UserService {
     ];
 
     // 获取所有用户
-    getUsers () {
+    getUsers() {
         return this.users;
     }
 
     // 根据 ID 获取用户
-    getUserById ( id: number ) {
-        return this.users.find( ( u ) => u.id === id );
+    getUserById(id: number) {
+        return this.users.find((u) => u.id === id);
     }
 
     // 创建用户
-    createUser ( user: { name: string } ) {
+    createUser(user: { name: string }) {
         const newUser = { id: this.users.length + 1, ...user };
-        this.users.push( newUser );
+        this.users.push(newUser);
         return newUser;
     }
 }
 
 // -------------------------- 示例：定义控制器 --------------------------
-@Controller( '/users' ) // 路由前缀
+@Controller('/users') // 路由前缀
 class UserController {
     // 从 DI 容器自动注入 UserService（逻辑不变）
-    constructor ( private userService: UserService ) {
-    }
+    constructor(private userService: UserService) {}
 
     // GET /users
-    @Get( '/' )
-    getUsers () {
-        console.log( '获取用户列表' );
+    @Get('/')
+    getUsers() {
+        console.log('获取用户列表');
         return this.userService.getUsers();
     }
 
     // GET /users/:id
-    @Get( '/:id' )
-    getUserById ( @Param( 'id' ) id: string, @Ctx() ctx: Context ) {
+    @Get('/:id')
+    getUserById(@Param('id') id: string, @Ctx() ctx: Context) {
         // 解析参数并调用服务
-        const user = this.userService.getUserById( Number( id ) );
-        if ( !user ) {
+        const user = this.userService.getUserById(Number(id));
+        if (!user) {
             ctx.status = 404;
             return { message: '用户不存在' };
         }
@@ -53,19 +52,19 @@ class UserController {
     }
 
     // POST /users
-    @Post( '/' )
-    createUser ( @Body() body: { name: string } ) {
-        return this.userService.createUser( body );
+    @Post('/')
+    createUser(@Body() body: { name: string }) {
+        return this.userService.createUser(body);
     }
 }
 
 const app = new KoaContainerApp();
-app.use(bodyParser())
+app.use(bodyParser());
 
-app.registerControllers( {
-    controllers: [ UserController ]
-} );
+app.registerControllers({
+    controllers: [UserController]
+});
 const port = 3000;
-app.listen( port, () => {
-    console.log( 'http://localhost:' + port );
-} );
+app.listen(port, () => {
+    console.log('http://localhost:' + port);
+});
