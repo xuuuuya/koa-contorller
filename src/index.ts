03fd35c20589870e66ecd7dfb772976dfe9ab13a
xuuuuya <xuuuuya@163.com>
2026-01-16 17:56:00 +0800

    添加导出类型

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
 import { Controller, Service, Ctx, Get, Post, Query, Body, Param } from './decorators';
 import { KoaContainerApp } from './app';
+import {
+    ConstructorType,
+    ControllerType,
+    ServiceType,
+    Scope,
+    DependencyItem,
+    RouteMetadata,
+    ControllerMetadata,
+    ParamType,
+    ParamMetadata,
+    ServiceMetadata,
+    METADATA_KEYS,
+    paramsControlType
+} from './types';
+export {
+    ConstructorType,
+    ControllerType,
+    ServiceType,
+    Scope,
+    DependencyItem,
+    RouteMetadata,
+    ControllerMetadata,
+    ParamType,
+    ParamMetadata,
+    ServiceMetadata,
+    METADATA_KEYS,
+    paramsControlType
+};
 
 export { KoaContainerApp, Controller, Service, Ctx, Get, Post, Query, Body, Param };
